98880b2eaac0d65aab2cebf7a608bb7157b51d3f
Kenneth Wußmann <kenneth@wussmann.net>
2023-02-11 14:42:44 +0100

    Add jest

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+module.exports = {
+  collectCoverage: false,
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: [['lcov', { projectRoot: '../..' }]],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.test.ts', '!src/**/index.ts'],
+  projects: [
+    {
+      testEnvironment: 'node',
+      preset: 'ts-jest',
+      coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
+      displayName: 'unit',
+      testMatch: ['<rootDir>/**/*.test.ts'],
+      testPathIgnorePatterns: ['it.test.ts'],
+      setupFiles: ['<rootDir>/test/setupEnv.ts'],
+    },
+  ],
+};
