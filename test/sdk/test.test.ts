3d622c703b500f9dcea1de83e632ddbe7f02525c
Kenneth Wußmann <kenneth@wussmann.net>
2023-02-12 18:08:52 +0100

    Remove test

--- test/sdk/test.test.ts
+++
@@ -1,7 +0,0 @@
-import * as SimpleLogin from '../../src';
-
-describe('test', () => {
-  it('test', () => {
-    const api = new SimpleLogin.AccountApi(new SimpleLogin.SimpleLoginConfig({ apiKey: '' }));
-  });
-});
