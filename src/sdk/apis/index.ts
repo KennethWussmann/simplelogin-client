87de574f90b601d340f31472dca8fde4c262cb4d
Kenneth Wußmann <kenneth@wussmann.net>
2023-02-13 23:45:45 +0100

    Remove injected code from index.ts files (#14)

--- src/sdk/apis/index.ts
+++ src/sdk/apis/index.ts
@@ -1,10 +1,3 @@
-/* eslint-disable */
-// @ts-nocheck
-type RequestCredentials = any;
-type Response = any;
-type RequestInit = any;
-type FormData = any;
-type WindowOrWorkerGlobalScope = any;
 /* tslint:disable */
 /* eslint-disable */
 export * from './AccountApi';
