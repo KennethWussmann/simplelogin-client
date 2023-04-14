22af5c26dece7d6905a07091636b0c9da6d3d877
Kenneth Wußmann <kenneth@wussmann.net>
2023-04-14 23:22:59 +0200

    Update redoc

--- scripts/steps/buildDocs.ts
+++ scripts/steps/buildDocs.ts
@@ -7,7 +7,7 @@ export const buildDocs = async (oasPath: string) => {
   console.log(sectionHeader('📚 Building docs'));
   return Promise.all([
     $`npx typedoc`,
-    $`npx redoc-cli build ${oasPath} -o ${join(distRedocDir, 'index.html')}`,
+    $`npx @redocly/cli build-docs ${oasPath} -o ${join(distRedocDir, 'index.html')}`,
     deleteIfExists(join(distRedocDir, '.nojekyll')),
   ]);
 };
