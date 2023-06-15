d04ca16137ee66f39921be08889fef7a06a7e80d
Kenneth Wußmann <kenneth@wussmann.net>
2023-06-15 21:51:10 +0200

    Build docs before pages upload

--- scripts/task/index.ts
+++ scripts/task/index.ts
@@ -1,4 +1,5 @@
 export * as build from './build';
+export * as buildDocs from './buildDocs';
 export * as rebuild from './rebuild';
 export * as ci from './ci';
 export * as preCommit from './preCommit';
--- scripts/task/preCommit.ts
+++ scripts/task/preCommit.ts
@@ -1,7 +1,5 @@
-import { chalk } from 'zx';
 import { $ } from 'zx/core';
 
 export default async () => {
   await $`npx lint-staged`;
-  console.log(chalk.yellowBright('Remember to also commit the generated SDK and Docs! Thank you 🙏'));
 };
