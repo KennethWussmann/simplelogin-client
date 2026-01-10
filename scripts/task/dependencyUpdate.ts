885fcd09eca1903dad9b5bf4eaf574ace6c068ba
Kenneth Wußmann <kenneth@wussmann.net>
2026-01-10 17:27:55 +0100

    Add dependabot

--- scripts/task/dependencyUpdate.ts
+++
@@ -1,8 +0,0 @@
-import { $ } from 'zx/core';
-import { sectionHeader } from '../utils';
-
-export default async () => {
-  console.log(sectionHeader('🔃 Updating Dependencies'));
-  await $`pnpm npm-check-updates -u --filterVersion \"/^[~^<>]| - |\\.x$/\" --deep`;
-  await $`pnpm install --no-frozen-lockfile`;
-};
--- scripts/task/index.ts
+++ scripts/task/index.ts
@@ -1,6 +1,5 @@
 export * as build from './build';
 export * as buildDocs from './buildDocs';
 export * as ci from './ci';
-export * as dependencyUpdate from './dependencyUpdate';
 export * as rebuild from './rebuild';
 export * as test from './test';
